cbb3d39e7a9c079a2928803cdba8bf24bc6fdcd3
hadoopadministrator <shahidbangi790@gmail.com>
2026-06-24 17:27:05 +0530

    remove unused product feild and update intl package any to 0.20.2

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,4 +1,5 @@
 import 'package:grabitt/models/vendor_product.dart';
+
 class Product {
   final String id;
   final String name;
@@ -28,8 +29,43 @@ class Product {
     required this.image3,
     required this.image4,
     required this.image5,
-    required String productID,
   });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['ProductID']?.toString() ?? '',
+      name: json['ProductName']?.toString() ?? '',
+      categoryName: json['CategoryName']?.toString() ?? '',
+      description: json['description']?.toString() ?? '',
+      originalPrice: json['OriginalPrice']?.toString() ?? '0',
+      discountPrice: json['DiscountPrice']?.toString() ?? '0',
+      discountPercent: json['DiscountPercent']?.toString() ?? '0',
+      productImage: json['ProductImage']?.toString() ?? '',
+      image1: json['Image1']?.toString() ?? '',
+      image2: json['Image2']?.toString() ?? '',
+      image3: json['Image3']?.toString() ?? '',
+      image4: json['Image4']?.toString() ?? '',
+      image5: json['Image5']?.toString() ?? '',
+    );
+  }
+
+  factory Product.fromVendor(VendorProduct v) {
+    return Product(
+      id: v.id,
+      name: v.name,
+      categoryName: v.categoryName,
+      description: v.description,
+      originalPrice: v.originalPrice,
+      discountPrice: v.discountPrice,
+      discountPercent: v.discountPercent,
+      productImage: v.images.isNotEmpty ? v.images.first : '',
+      image1: v.images.length > 1 ? v.images[1] : '',
+      image2: v.images.length > 2 ? v.images[2] : '',
+      image3: v.images.length > 3 ? v.images[3] : '',
+      image4: v.images.length > 4 ? v.images[4] : '',
+      image5: v.images.length > 5 ? v.images[5] : '',
+    );
+  }
 
   Map<String, dynamic> toJson() => {
         'ProductID': id,
@@ -39,7 +75,7 @@ class Product {
         'DiscountPrice': discountPrice,
         'DiscountPercent': discountPercent,
         'ProductImage': productImage,
-        'description': description ?? "description",
+        'description': description ?? '',
         'Image1': image1,
         'Image2': image2,
         'Image3': image3,
@@ -47,59 +83,17 @@ class Product {
         'Image5': image5,
       };
 
-  factory Product.fromJson(Map<String, dynamic> json) {
-    return Product(
-      id: json['ProductID'] ?? '',
-      name: json['ProductName'] ?? '',
-      categoryName: json['CategoryName'] ?? '',
-      description: json['description'] ?? '',
-      originalPrice: json['OriginalPrice']?.toString() ?? '0',
-      discountPrice: json['DiscountPrice']?.toString() ?? '0',
-      discountPercent: json['DiscountPercent']?.toString() ?? '0',
-      productImage: json['ProductImage'] ?? '',
-      image1: json['Image1'] ?? '',
-      image2: json['Image2'] ?? '',
-      image3: json['Image3'] ?? '',
-      image4: json['Image4'] ?? '',
-      image5: json['Image5'] ?? '',
-      productID: json['ProductID'] ?? '',
-    );
-  }
-
-  factory Product.fromVendor(VendorProduct v) {
-    return Product(
-      productID: v.id,
-      name: v.name,
-      categoryName: v.categoryName,
-      description: v.description,
-      originalPrice: v.originalPrice,
-      discountPrice: v.discountPrice,
-      discountPercent: v.discountPercent,
-      productImage: v.images.isNotEmpty ? v.images.first : "",
-      image1: v.images.length > 1 ? v.images[1] : "",
-      image2: v.images.length > 2 ? v.images[2] : "",
-      image3: v.images.length > 3 ? v.images[3] : "",
-      image4: v.images.length > 4 ? v.images[4] : "",
-      id: v.id,
-      image5: v.images.length >5 ? v.images[5] :"",
-    );
-  }
-
-
-  /// 🔥 Helper to get all images safely
+  /// All non-empty images with resolved URLs.
   List<String> get allImages => [
-    productImage,
-    image1,
-    image2,
-    image3,
-    image4,
-    image5,
-  ]
-      .where((img) => img.isNotEmpty)
-      .map((img) => img.startsWith("http")
-      ? img
-      : "https://grabitt.in/$img")
-      .toList();
-
+        productImage,
+        image1,
+        image2,
+        image3,
+        image4,
+        image5,
+      ]
+          .where((img) => img.isNotEmpty)
+          .map(
+              (img) => img.startsWith('http') ? img : 'https://grabitt.in/$img')
+          .toList();
 }
-
